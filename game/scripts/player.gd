6630a5194ea071ef640afe19baaaf6011527be06
theowiik <42991003+theowiik@users.noreply.github.com>
2025-12-23 15:15:44 +0100

    Use player model (#14)
    * Use player model in sprites
    
    * Clean up player code
    
    * Fix player aiming

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,16 +1,20 @@
 extends CharacterBody3D
 
+@onready var aim_pivot: Node3D = $AimPivot
+@onready var shoot_position: Node3D = $AimPivot/ShootPosition
+
 const team_color: Color = Color(1, 0.4, 0.2, 1)
 
-var facing_direction: int = 1
-
+# Controller
 @export var device_id: int = 0
 var input: InputInterface
 
+# Movement
 var mover: MoverInterface
 var base_mover: MoverInterface
 var mover_decorators: Array[String] = []
 
+# Shooting
 var shooter: ShooterInterface
 var base_shooter: ShooterInterface
 var shooter_decorators: Array[String] = []
@@ -29,78 +33,34 @@ func _ready():
 
 func _physics_process(delta):
 	input.update()
+	_process_shooting(delta)
+	_process_movement(delta)
 
+
+func _process_shooting(delta: float) -> void:
 	if shoot_cooldown > 0:
 		shoot_cooldown -= delta
 
+	var aim_input = input.get_aim_direction()
+	if aim_input.length() > 0:
+		var angle = -atan2(aim_input.y, aim_input.x)
+		aim_pivot.rotation.z = angle
+
 	if input.is_shoot_pressed() and shoot_cooldown <= 0:
-		shoot()
+		_shoot()
 		shoot_cooldown = shooter.get_shoot_delay()
 
-	var input_vector = input.get_movement()
+
+func _process_movement(delta: float) -> void:
 	var context = {
 		"velocity_y": velocity.y,
 		"is_on_floor": is_on_floor(),
 		"jump_pressed": input.is_jump_just_pressed()
 	}
-	velocity = mover.process_movement(input_vector, delta, context)
-
-	var aim = input.get_aim_direction()
-	if aim.x != 0:
-		facing_direction = 1 if aim.x > 0 else -1
-	elif input_vector.x > 0:
-		facing_direction = 1
-	elif input_vector.x < 0:
-		facing_direction = -1
-
+	velocity = mover.process_movement(input.get_movement(), delta, context)
 	move_and_slide()
 
 
-func toggle_mover_decorator(decorator_name: String):
-	if decorator_name in mover_decorators:
-		mover_decorators.erase(decorator_name)
-	else:
-		mover_decorators.append(decorator_name)
-	rebuild_mover()
-
-
-func toggle_shooter_decorator(decorator_name: String):
-	if decorator_name in shooter_decorators:
-		shooter_decorators.erase(decorator_name)
-	else:
-		shooter_decorators.append(decorator_name)
-	rebuild_shooter()
-
-
-func rebuild_mover():
-	mover = base_mover
-	for decorator_name in mover_decorators:
-		match decorator_name:
-			"phast":
-				mover = PhastDecorator.new(mover)
-
-
-func rebuild_shooter():
-	shooter = base_shooter
-	for decorator_name in shooter_decorators:
-		match decorator_name:
-			"triple_shot":
-				shooter = TripleShotDecorator.new(shooter)
-			"fast_bullets":
-				shooter = FastBulletsDecorator.new(shooter)
-			"rapid_fire":
-				shooter = RapidFireDecorator.new(shooter)
-
-
-func shoot():
-	var direction = get_aim_direction()
-	shooter.shoot(global_position, direction, team_color, get_parent())
-
-
-func get_aim_direction() -> Vector3:
-	var aim_2d = input.get_aim_direction()
-
-	if aim_2d != Vector2.ZERO:
-		return Vector3(aim_2d.x, -aim_2d.y, 0).normalized()
-
-	return Vector3.RIGHT * facing_direction
+func _shoot():
+	var direction = (shoot_position.global_position - global_position).normalized()
+	shooter.shoot(shoot_position.global_position, direction, team_color, get_parent())
