b3e25db6be63ea86818e657c197ed547b203aabe
theowiik <42991003+theowiik@users.noreply.github.com>
2025-12-26 18:08:38 +0100

    Add player animation manager (#31)
    * Format
    
    * Add platform scene
    
    * Interpolate between animations

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -20,12 +20,12 @@ var shooter: ShooterInterface
 var base_shooter: ShooterInterface
 var shooter_decorator_names: Array[String] = []
 var shoot_cooldown: float = 0.0
+var animation_manager: AnimationManager
 
 @onready var aim_pivot: Node3D = $AimPivot
 @onready var shoot_position: Node3D = $AimPivot/ShootPosition
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-var animation_manager: AnimationManager
+@onready var body: Node3D = $Body
 
 
 func _ready():
@@ -37,7 +37,7 @@ func _ready():
 	base_shooter = BaseShooter.new()
 	shooter = base_shooter
 
-	animation_manager = AnimationManager.new(animation_player)
+	animation_manager = AnimationManager.new(animation_player, body)
 
 
 func _physics_process(delta):
@@ -80,7 +80,7 @@ func _process_movement(delta: float) -> void:
 		external_velocity = Vector3.ZERO
 
 	move_and_slide()
-	animation_manager.update(velocity.y, is_on_floor())
+	animation_manager.update(velocity, is_on_floor())
 
 
 func apply_impulse(impulse: Vector3) -> void:
