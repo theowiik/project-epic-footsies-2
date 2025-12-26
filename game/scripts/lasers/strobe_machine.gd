a17357a9b3dfc5006c9483d48cfa0620be4137d2
Theo Wiik <theo.le.wiik@gmail.com>
2025-12-26 18:23:19 +0100

    Add strobe and laser machine

---
+++ game/scripts/lasers/strobe_machine.gd
@@ -0,0 +1,21 @@
+class_name StrobeMachine
+extends Node3D
+
+var on: bool = false
+var strobe_speed: float = 10.0
+
+@onready var light: SpotLight3D = $SpotLight3D
+
+
+func _process(_delta: float) -> void:
+	if not on:
+		return
+
+	var time = Time.get_ticks_msec() / 1000.0
+	light.visible = sin(time * strobe_speed * TAU) > 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_J:
+			on = !on
