b37339a8d032d8f3e3fff45f6b27883ebfdc9522
Theo Wiik <theo.le.wiik@gmail.com>
2025-12-26 17:50:05 +0100

    Format

--- game/scripts/animation/animation_manager.gd
+++ game/scripts/animation/animation_manager.gd
@@ -1,13 +1,7 @@
 class_name AnimationManager
 extends RefCounted
 
-enum State {
-	GROUNDED,
-	INITIATE_JUMP,
-	JUMP_UP,
-	JUMP_DOWN,
-	LANDING
-}
+enum State { GROUNDED, INITIATE_JUMP, JUMP_UP, JUMP_DOWN, LANDING }
 
 var animation_player: AnimationPlayer
 var current_state: State = State.GROUNDED
@@ -20,10 +14,10 @@ func _init(anim_player: AnimationPlayer):
 
 func update(velocity_y: float, is_on_floor: bool) -> void:
 	var new_state = _determine_state(velocity_y, is_on_floor)
-	
+
 	if new_state != current_state:
 		_transition_to(new_state)
-	
+
 	was_on_floor = is_on_floor
 
 
@@ -34,24 +28,24 @@ func _determine_state(velocity_y: float, is_on_floor: bool) -> State:
 		if current_state == State.LANDING and animation_player.is_playing():
 			return State.LANDING
 		return State.GROUNDED
-	
+
 	if current_state == State.GROUNDED and velocity_y > 0:
 		return State.INITIATE_JUMP
-	
+
 	if current_state == State.INITIATE_JUMP:
 		if animation_player.is_playing():
 			return State.INITIATE_JUMP
 		return State.JUMP_UP
-	
+
 	if velocity_y > 0:
 		return State.JUMP_UP
-	
+
 	return State.JUMP_DOWN
 
 
 func _transition_to(new_state: State) -> void:
 	current_state = new_state
-	
+
 	match new_state:
 		State.GROUNDED:
 			animation_player.play("walk")
@@ -63,4 +57,3 @@ func _transition_to(new_state: State) -> void:
 			animation_player.play("jump_down")
 		State.LANDING:
 			animation_player.play("jump_land")
-
