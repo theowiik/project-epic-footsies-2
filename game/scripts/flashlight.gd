18bc1a5aa49700e77f02901649b4aa90b68f51d2
Theo Wiik <theo.le.wiik@gmail.com>
2025-12-27 11:25:34 +0100

    Simplify flashlight meshes

--- game/scripts/flashlight.gd
+++ game/scripts/flashlight.gd
@@ -7,10 +7,8 @@ const ROTATION_SPEED: float = 90.0
 
 var target_position: Node3D = null
 var aim_origin: Node3D = null
-var facing_right: bool = true
 
-@onready var flashlight_model: Node3D = $FlashlightOnly
-@onready var hand_model: Node3D = $HandOnly
+@onready var hand_mesh: MeshInstance3D = $Visuals/HandOnly/flashlight_hand
 @onready var shoot_position: Node3D = $ShootPosition
 
 
@@ -21,17 +19,22 @@ func _ready() -> void:
 
 
 func set_color(color: Color) -> void:
-	var material = hand_model.get_node("flashlight_hand").get_active_material(0)
+	var material = hand_mesh.get_active_material(0)
 	if material:
 		material = material.duplicate()
 		material.albedo_color = color
-		hand_model.get_node("flashlight_hand").set_surface_override_material(0, material)
+		hand_mesh.set_surface_override_material(0, material)
 
 
 func _physics_process(delta: float) -> void:
+	_move_towards_target_position(delta)
+
+
+func _move_towards_target_position(delta: float) -> void:
 	if target_position == null or aim_origin == null:
 		return
 
+	# Position
 	var target_pos = target_position.global_position
 	var current_pos = global_position
 	var displacement = target_pos - current_pos
@@ -42,20 +45,8 @@ func _physics_process(delta: float) -> void:
 
 	apply_central_force(force)
 
+	# Rotation
 	var aim_direction = (target_position.global_position - aim_origin.global_position).normalized()
-
-	_update_facing(aim_direction.x)
 
 	var target_basis = Basis.looking_at(aim_direction, Vector3.UP)
 	global_transform.basis = global_transform.basis.slerp(target_basis, ROTATION_SPEED * delta)
-
-
-func _update_facing(aim_x: float) -> void:
-	if aim_x > 0.1 and not facing_right:
-		facing_right = true
-		flashlight_model.scale.x = 1.0
-		hand_model.scale.x = 1.0
-	elif aim_x < -0.1 and facing_right:
-		facing_right = false
-		flashlight_model.scale.x = -1.0
-		hand_model.scale.x = -1.0
