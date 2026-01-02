52b42c10ff4b22d9bd16265b5f746e7ea954f232
theowiik <42991003+theowiik@users.noreply.github.com>
2026-01-02 18:22:14 +0100

    Improve bulb spawning (#48)
    * Improve bulb spawning robustnes
    
    * Fix ordering

--- game/scripts/bullet.gd
+++ game/scripts/bullet.gd
@@ -8,6 +8,7 @@ var speed: float = 10.0
 var lifetime: float = 3.0
 var _gravity: float = 3
 var _material: StandardMaterial3D
+var _hit: bool = false
 
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 @onready var light: OmniLight3D = $OmniLight3D
@@ -21,7 +22,8 @@ func _ready():
 
 	_apply_team_color()
 	await get_tree().create_timer(lifetime).timeout
-	queue_free()
+	if is_inside_tree():
+		queue_free()
 
 
 func _physics_process(delta):
@@ -43,8 +45,14 @@ func _apply_team_color() -> void:
 
 
 func _on_body_entered(_body: Node3D) -> void:
+	if _hit or not is_inside_tree():
+		return
+	_hit = true
+	set_deferred("monitoring", false)
+
+	var spawn_pos := global_position
 	var bulb = BULB_SCENE.instantiate()
 	bulb.set_team(team)
-	bulb.global_position = global_position
 	get_tree().current_scene.add_child(bulb)
+	bulb.global_position = spawn_pos
 	queue_free()
