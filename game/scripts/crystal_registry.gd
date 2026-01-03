a6f0b3f654ae88255a3dd7e7de7544472a36520f
Theo Wiik <theo.le.wiik@gmail.com>
2026-01-03 12:42:11 +0100

    Format

--- game/scripts/crystal_registry.gd
+++ game/scripts/crystal_registry.gd
@@ -1,12 +1,12 @@
 class_name CrystalRegistry
 extends RefCounted
 
-static var MOVEMENT_CRYSTALS = {
+static var movement_crystals = {
 	"phast": PhastDecorator,
 	"double_jump": DoubleJumpDecorator,
 }
 
-static var SHOOTING_CRYSTALS = {
+static var shooting_crystals = {
 	"rapid_fire": RapidFireDecorator,
 	"triple_shot": TripleShotDecorator,
 	"fast_bullets": FastBulletsDecorator,
@@ -16,29 +16,29 @@ static var SHOOTING_CRYSTALS = {
 
 static func get_all_crystals() -> Array[String]:
 	var all: Array[String] = []
-	all.append_array(MOVEMENT_CRYSTALS.keys())
-	all.append_array(SHOOTING_CRYSTALS.keys())
+	all.append_array(movement_crystals.keys())
+	all.append_array(shooting_crystals.keys())
 	return all
 
 
 static func is_movement_crystal(crystal_name: String) -> bool:
-	return MOVEMENT_CRYSTALS.has(crystal_name)
+	return movement_crystals.has(crystal_name)
 
 
 static func is_shooting_crystal(crystal_name: String) -> bool:
-	return SHOOTING_CRYSTALS.has(crystal_name)
+	return shooting_crystals.has(crystal_name)
 
 
 static func create_movement_decorator(crystal_name: String, base: MoverInterface) -> MoverInterface:
-	if not MOVEMENT_CRYSTALS.has(crystal_name):
+	if not movement_crystals.has(crystal_name):
 		return null
-	return MOVEMENT_CRYSTALS[crystal_name].new(base)
+	return movement_crystals[crystal_name].new(base)
 
 
 static func create_shooting_decorator(
 	crystal_name: String, base: ShooterInterface
 ) -> ShooterInterface:
-	if not SHOOTING_CRYSTALS.has(crystal_name):
+	if not shooting_crystals.has(crystal_name):
 		return null
 
-	return SHOOTING_CRYSTALS[crystal_name].new(base)
+	return shooting_crystals[crystal_name].new(base)
